dfa25f17658e9819dcacf050a5b0ef282846a79d
Rythix <achilles_104@aol.com>
2014-01-24 16:43:34 -0500

    Added configuration

--- src/assets/api.php
+++ src/assets/api.php
@@ -1,11 +1,16 @@
 <?php
 	session_start();
 	
+	// Include the configuration file
+	include("config.php");
+	
 	if(isset($_GET["a"])) {
 		$a = $_GET["a"];
 		
 		switch($a) {
 			case "login":
+				$usr = stripslashes($_POST["username"));
+				$psw = stripslashes(sha1(md5($_POST["password"] + $login_salt)));
 				break;
 			case "logout":
 				session_destroy();
---
+++ src/assets/config.php
@@ -0,0 +1,11 @@
+<?php
+	$db_host = "localhost"; // The host of the MySQL server that contains the database;
+	$db_username = "root"; // The username that will be used to connect to the database;
+	$db_password = ""; // The password that will be used to connect to the database;
+	$db = ""; // The database that will be used for connections;
+	
+	//
+	
+	$login_salt = "tastysalt"; /* This should be changed as it is used to hash the passwords; NOTICE : ACCOUNTS ADDED BEFORE THIS WILL NEED THEIR
+	                  PASSWORDS RESET; */
+?>
