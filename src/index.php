9a8c6bc939bace36465b52de687c4d4f7a105331
Rythix <achilles_104@aol.com>
2014-01-30 19:06:08 -0500

    Added Bootstrap

--- src/index.php
+++ src/index.php
@@ -9,11 +9,13 @@
 	<head>
 		<title>Home — Study Mountain</title>
 		<link type="text/css" rel="stylesheet" href="assets/css/main.css" />
+		<link type="text/css" rel="stylesheet" href="assets/css/bootstrap.css" />
+		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
 	</head>
 	
 	<body>
 		<h1>Please login below</h1>
-		<form name="login" method="post" action="/assets/api.php?a=login">
+		<form name="login" method="post" action="assets/api.php?a=login">
 			<input type="text" name="username" placeholder="Username" />
 			<br />
 			<input type="password" name="password" placeholder="Password" />
