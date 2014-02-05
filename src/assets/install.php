014ed7f5256654c69f9797d1b888cbfdd1e3deb4
Rythix <Rythix@users.noreply.github.com>
2014-02-05 14:46:16 -0500

    Update install.php

--- src/assets/install.php
+++ src/assets/install.php
@@ -3,4 +3,24 @@
 		echo "Already installed!";
 		exit;
 	}
+	
+	if(isset($_GET["pid"])) {
+		$page = $_GET["pid"];
+	} else {
+		// Assuming this is the first part of the installation
+		$page = "1";
+	}
+	
 ?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Install - StudyMountain</title>
+	</head>
+	
+	<body>
+	
+	</body>
+
+</html>
