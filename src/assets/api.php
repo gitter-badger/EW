f0b3eccd225459dc02ae834d7c93e62c54316dff
Rythix <achilles_104@aol.com>
2014-03-09 14:05:14 -0400

    Finish up the login system

--- src/assets/api.php
+++ src/assets/api.php
@@ -13,7 +13,26 @@
 				$usr = stripslashes($_POST["username"]);
 				$psw = stripslashes(sha1(md5($_POST["password"] + $login_salt)));
 				
+				$query = 'SELECT * FROM users WHERE username="' . $usr . '" and password="'. $psw . '";';
+				
 				$mysqli = new mysqli($db_host, $db_username, $db_password, $db);
+				if (mysqli_connect_errno()) {
+					trigger_error('Authentication Failed : ' . mysqli_connect_error(), E_USER_ERROR);
+				}
+				
+				$result = mysqli_query($mysqli, $query);
+				
+				$count = mysqli_num_rows($result);
+				
+				if($count == 1) {
+					$_SESSION['login'] = "1";
+					$_SESSION['username'] = $_POST["username"];
+					// TODO: Work on dashboard
+					exit;
+				} else {
+					header("Location:" . $main_url . "/?msg=Incorrect+username+or+password");
+					exit;
+				}
 				
 				break;
 			case "logout":
