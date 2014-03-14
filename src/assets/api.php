e7d63aaf687c775bccaae6e34d340f838c855a72
Rythix <achilles_104@aol.com>
2014-03-14 18:27:41 -0400

    Added a login check to the API

--- src/assets/api.php
+++ src/assets/api.php
@@ -10,30 +10,31 @@
 		
 		switch($a) {
 			case "login":
-				$usr = stripslashes($_POST["username"]);
-				$psw = stripslashes(sha1(md5($_POST["password"] + $login_salt)));
+				if(!(isset($_SESSION["login"])) || !(isset($_SESSION["username"]))) {
+					$usr = stripslashes($_POST["username"]);
+					$psw = stripslashes(sha1(md5($_POST["password"] + $login_salt)));
 				
-				$query = 'SELECT * FROM users WHERE username="' . $usr . '" and password="'. $psw . '";';
+					$query = 'SELECT * FROM users WHERE username="' . $usr . '" and password="'. $psw . '";';
 				
-				$mysqli = new mysqli($db_host, $db_username, $db_password, $db);
-				if (mysqli_connect_errno()) {
-					trigger_error('Authentication Failed : ' . mysqli_connect_error(), E_USER_ERROR);
+					$mysqli = new mysqli($db_host, $db_username, $db_password, $db);
+					if (mysqli_connect_errno()) {
+						trigger_error('Authentication Failed : ' . mysqli_connect_error(), E_USER_ERROR);
+					}
+				
+					$result = mysqli_query($mysqli, $query);
+					
+					$count = mysqli_num_rows($result);
+				
+					if($count == 1) {
+						$_SESSION['login'] = "1";
+						$_SESSION['username'] = $_POST["username"];
+						header("Location: dashboard.php?PHPSESSID=" . session_id());
+						exit;
+					} else {
+						header("Location:" . $main_url . "/?msg=Incorrect+username+or+password");
+						exit;
+					}
 				}
-				
-				$result = mysqli_query($mysqli, $query);
-				
-				$count = mysqli_num_rows($result);
-				
-				if($count == 1) {
-					$_SESSION['login'] = "1";
-					$_SESSION['username'] = $_POST["username"];
-					header("Location: dashboard.php?PHPSESSID=" . session_id());
-					exit;
-				} else {
-					header("Location:" . $main_url . "/?msg=Incorrect+username+or+password");
-					exit;
-				}
-				
 				break;
 			case "logout":
 				session_destroy();
